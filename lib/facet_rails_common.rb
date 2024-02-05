76f152dfbade498044ab61f008570ff53c9c8908
Tom Lehman <tom.s.lehman@gmail.com>
2024-02-05 16:57:39 -0500

    Add more extensions

--- lib/facet_rails_common.rb
+++ lib/facet_rails_common.rb
@@ -2,6 +2,8 @@
 
 require_relative "facet_rails_common/version"
 require_relative "facet_rails_common/numbers_to_strings"
+require_relative "facet_rails_common/string_extensions"
+require_relative "facet_rails_common/console_extensions"
 require_relative "facet_rails_common/application_controller_methods"
 
 if defined?(FacetVmClient)
