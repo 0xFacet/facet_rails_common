bc1fde6a32b08ff6e1aac328325754a54da12834
Tom Lehman <tom.s.lehman@gmail.com>
2024-02-05 17:53:21 -0500

    Update VM client

--- lib/facet_rails_common/facet_vm_client.rb
+++ lib/facet_rails_common/facet_vm_client.rb
@@ -18,6 +18,11 @@ module ::FacetVmClient
     res = make_request(url)['result']
   end
   
+  def self.get_transactions(**kwargs)
+    url = "#{base_url}/transactions"
+    make_request_with_pagination(url, kwargs)
+  end
+
   def self.get_status
     url = "#{base_url}/status"
     make_request(url).deep_transform_values(&:to_i)
@@ -39,10 +44,27 @@ module ::FacetVmClient
     end
   end
   
+  def self.make_request_with_pagination(url, query = {}, method: :get, post_body: nil, timeout: 5, max_results: nil)
+    results = []
+    page_key = nil
+  
+    loop do
+      response = make_request(url, query.merge(page_key: page_key), method: method, post_body: post_body, timeout: timeout)
+      results.concat(response['result'])
+      break if !response['pagination']['has_more'] || (max_results && results.size >= max_results)
+      
+      page_key = response['pagination']['page_key']
+    end
+  
+    results
+  end
+  
   def self.make_request(url, query = {}, method: :get, post_body: nil, timeout: 5)
     headers = {}
     headers['Authorization'] = "Bearer #{bearer_token}" if bearer_token
     
+    query.merge!(user_cursor_pagination: true) if method == :get
+        
     res = begin
       response = HTTParty.send(method, url, { query: query, headers: headers, timeout: timeout, body: post_body }.compact)
       
