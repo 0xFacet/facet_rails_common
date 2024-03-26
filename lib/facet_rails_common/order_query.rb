7396f1b14febc455315db727e1280bb7266ac360
Tom Lehman <tom.s.lehman@gmail.com>
2024-03-26 18:04:36 -0400

    DRY order query setup

--- lib/facet_rails_common/order_query.rb
+++ lib/facet_rails_common/order_query.rb
@@ -4,11 +4,27 @@ module FacetRailsCommon::OrderQuery
   extend ActiveSupport::Concern
   include ::OrderQuery
 
+  class InvalidOrderQueryConfig < StandardError; end
+
   included do
     class_attribute :order_query_scopes, default: []
+    class_attribute :page_key_attributes
   end
 
   class_methods do
+    # Modified to accept a hash of order queries and a single set of key attributes
+    def initialize_order_query(order_specs, page_key_attributes:)
+      if page_key_attributes.blank?
+        raise InvalidOrderQueryConfig, "page_key_attributes must be present"
+      end
+      
+      self.page_key_attributes = page_key_attributes
+
+      order_specs.each do |name, order_spec|
+        order_query(name, *order_spec)
+      end
+    end
+
     def order_query(name, *spec)
       self.order_query_scopes |= [name.dup]
 
@@ -19,5 +35,17 @@ module FacetRailsCommon::OrderQuery
       return false unless name.present?
       order_query_scopes.include?(name.to_sym)
     end
+
+    # Unified find_by_page_key method based on class-wide key attributes
+    def find_by_page_key(key)
+      key_values = key.split("-")
+      key_hash = page_key_attributes.zip(key_values).to_h
+      find_by(key_hash)
+    end
+  end
+
+  # Instance method for generating a page key based on class-wide key attributes
+  def page_key
+    self.class.page_key_attributes.map { |attr| public_send(attr) }.join("-")
   end
 end
